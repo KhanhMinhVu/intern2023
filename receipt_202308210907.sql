3ad47e5e68658bff6d47f0451f14437f7a198d4b
KhanhMinhVu <91825054+KhanhMinhVu@users.noreply.github.com>
2023-08-21 09:08:20 +0700

    add Database

---
+++ receipt_202308210907.sql
@@ -0,0 +1,33 @@
+INSERT INTO bangao.receipt (active,create_date,update_data,good_rices) VALUES
+	 (1,NULL,NULL,81),
+	 (1,NULL,NULL,93),
+	 (1,NULL,NULL,111),
+	 (1,NULL,NULL,111),
+	 (1,NULL,NULL,93),
+	 (1,NULL,NULL,57),
+	 (1,NULL,NULL,105),
+	 (1,NULL,NULL,96),
+	 (1,NULL,NULL,36),
+	 (1,NULL,NULL,72);
+INSERT INTO bangao.receipt (active,create_date,update_data,good_rices) VALUES
+	 (1,NULL,NULL,117),
+	 (1,NULL,NULL,18),
+	 (1,NULL,NULL,66),
+	 (1,NULL,NULL,36),
+	 (1,NULL,NULL,15),
+	 (1,NULL,NULL,27),
+	 (1,NULL,NULL,27),
+	 (1,NULL,NULL,69),
+	 (1,NULL,NULL,57),
+	 (1,NULL,NULL,93);
+INSERT INTO bangao.receipt (active,create_date,update_data,good_rices) VALUES
+	 (1,NULL,NULL,69),
+	 (1,NULL,NULL,33),
+	 (1,NULL,NULL,114),
+	 (1,NULL,NULL,48),
+	 (1,NULL,NULL,39),
+	 (1,NULL,NULL,78),
+	 (1,NULL,NULL,90),
+	 (1,NULL,NULL,18),
+	 (1,NULL,NULL,36),
+	 (1,NULL,NULL,36);
